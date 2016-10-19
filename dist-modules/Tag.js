41149f4d87a7244523cab6cbec887fb9f9baad08
Ryan Gaus <rsg1egoman@gmail.com>
2016-10-19 10:31:23 -0400

    code: make tags colorful!

--- dist-modules/Tag.js
+++ dist-modules/Tag.js
@@ -14,6 +14,10 @@ var _lodash = require('lodash.flow');
 
 var _lodash2 = _interopRequireDefault(_lodash);
 
+var _colorHash = require('color-hash');
+
+var _colorHash2 = _interopRequireDefault(_colorHash);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -21,6 +25,8 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var colorHasher = new _colorHash2.default();
 
 var ItemTypes = { TAG: 'tag' };
 
@@ -62,7 +68,7 @@ var Tag = function (_Component) {
   _inherits(Tag, _Component);
 
   function Tag() {
-    var _Object$getPrototypeO;
+    var _ref;
 
     var _temp, _this, _ret;
 
@@ -72,7 +78,7 @@ var Tag = function (_Component) {
       args[_key] = arguments[_key];
     }
 
-    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_Object$getPrototypeO = Object.getPrototypeOf(Tag)).call.apply(_Object$getPrototypeO, [this].concat(args))), _this), _this.render = function () {
+    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Tag.__proto__ || Object.getPrototypeOf(Tag)).call.apply(_ref, [this].concat(args))), _this), _this.render = function () {
       var _this2 = _this;
       var props = _this2.props;
 
@@ -101,7 +107,7 @@ var Tag = function (_Component) {
       });
       var tagComponent = _react2.default.createElement(
         'span',
-        { style: { opacity: isDragging ? 0 : 1 }, className: props.classNames.tag },
+        { style: { opacity: isDragging ? 0 : 1, background: colorHasher.hex(label) }, className: props.classNames.tag },
         label,
         _react2.default.createElement(RemoveComponent, { className: props.classNames.remove, onClick: props.onDelete })
       );
